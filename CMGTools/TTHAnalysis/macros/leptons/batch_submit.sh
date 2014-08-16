1bee61891ebdab278be4c6839f624582cc5cc998
cirkovic <predrag.cirkovic@cern.ch>
2014-08-16 13:27:35 +0200

    batch_submit.sh changed to 8nh queue.

--- CMGTools/TTHAnalysis/macros/leptons/batch_submit.sh
+++ CMGTools/TTHAnalysis/macros/leptons/batch_submit.sh
@@ -3,6 +3,6 @@
 for arg in mu_pteta_high_b mu_pteta_low_b mu_pteta_high_e mu_pteta_low_e el_pteta_high_cb el_pteta_low_cb el_pteta_high_fb el_pteta_low_fb el_pteta_high_ec el_pteta_low_ec
 do
    chmod +x submit_script.sh
-   bsub -q 1nh submit_script.sh $arg
+   bsub -q 8nh submit_script.sh $arg
 done
 
