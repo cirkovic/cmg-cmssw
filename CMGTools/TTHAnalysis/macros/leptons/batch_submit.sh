9b003a98d032e938fe76ccc61c386bda3b3ade31
cirkovic <predrag.cirkovic@cern.ch>
2014-08-16 13:21:37 +0200

    Added batch_submit.sh and submit_script.sh for trainLeptonID.cxx

---
+++ CMGTools/TTHAnalysis/macros/leptons/batch_submit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for arg in mu_pteta_high_b mu_pteta_low_b mu_pteta_high_e mu_pteta_low_e el_pteta_high_cb el_pteta_low_cb el_pteta_high_fb el_pteta_low_fb el_pteta_high_ec el_pteta_low_ec
+do
+   chmod +x submit_script.sh
+   bsub -q 1nh submit_script.sh $arg
+done
+
