ddbf07578240f4c7f9b41848f8b41f0d0a27a1b2
cirkovic <predrag.cirkovic@cern.ch>
2014-08-18 21:28:54 +0200

    Changed sumbit_script.sh changed to use the ROOT v5.34

--- CMGTools/TTHAnalysis/macros/leptons/submit_script.sh
+++ CMGTools/TTHAnalysis/macros/leptons/submit_script.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-cd /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/CMSSW_5_3_19/src
+#cd /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/CMSSW_5_3_19/src
+cd /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/CMSSW_7_0_6_patch1/src
 eval `scramv1 runtime -sh`
 cd /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/CMSSW_5_3_19/src/CMGTools/TTHAnalysis/macros/leptons
 root -l -q 'trainLeptonID.cxx("'$1'")'
