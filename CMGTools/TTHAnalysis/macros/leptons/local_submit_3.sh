b26f936c9442102ea56b67e3f4eda70504ac5079
cirkovic <predrag.cirkovic@cern.ch>
2014-08-19 01:43:30 +0200

    Scripts for the local running of the trainLeptonID.cxx added.

---
+++ CMGTools/TTHAnalysis/macros/leptons/local_submit_3.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+source /afs/cern.ch/sw/lcg/external/gcc/4.9.1/x86_64-slc6-gcc48-opt/setup.sh
+source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.20/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
+
+#arg=mu_pteta_high_b
+#arg=mu_pteta_low_b
+arg=mu_pteta_high_e
+#arg=mu_pteta_low_e
+#arg=el_pteta_high_cb
+#arg=el_pteta_low_cb
+#arg=el_pteta_high_fb
+#arg=el_pteta_low_fb
+#arg=el_pteta_high_ec
+#arg=el_pteta_low_ec
+
+root -l -q 'trainLeptonID.cxx("'$arg'")'
+
