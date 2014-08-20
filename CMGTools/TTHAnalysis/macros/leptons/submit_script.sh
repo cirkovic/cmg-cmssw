c1648460f97950e06a11a58f5f1323bf8cc24f27
cirkovic <predrag.cirkovic@cern.ch>
2014-08-20 19:25:18 +0200

    batch_submit.sh and submit_script.sh scripts added to use batch for the trainLeptonID.cxx script.

---
+++ CMGTools/TTHAnalysis/macros/leptons/submit_script.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/CMSSW_5_3_19/src
+eval `scramv1 runtime -sh`
+cd /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/CMSSW_5_3_19/src/CMGTools/TTHAnalysis/macros/leptons
+root -l -q 'trainLeptonID.cxx("'$1'")'
+
