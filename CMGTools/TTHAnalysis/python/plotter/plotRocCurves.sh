97b1ffca95f8b1daea5dac2141e0b4e4f5c3400e
cirkovic <predrag.cirkovic@cern.ch>
2014-08-20 22:43:26 +0200

    plotRocCurves.sh written in order to parallelize plotting ROC curves.

---
+++ CMGTools/TTHAnalysis/python/plotter/plotRocCurves.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+cd /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/CMSSW_5_3_19/src
+eval `scramv1 runtime -sh`
+cd /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/CMSSW_5_3_19/src/CMGTools/TTHAnalysis/python/plotter
+
+case $1 in
+   1 )
+		python rocCurves.py -P /afs/cern.ch/work/g/gpetrucc/TREES_53X_170714 -j 6 -f -l 19.6 mca-2lss-dataBCat4Plots.txt bins/2lss_mumu.txt roc_2lss.txt -F sf/t /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/20-08-2014/3/1/evVarFriend_{cname}.root --tree treeProducerSusyMultilepton --s2v
+		mv roc_2lss_mumu.png roc_2lss_mumu.root /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/20-08-2014/4/1/1
+		;;
+	2 )
+		python rocCurves.py -P /afs/cern.ch/work/g/gpetrucc/TREES_53X_170714 -j 6 -f -l 19.6 mca-2lss-dataBCat4Plots.txt bins/2lss_em.txt roc_2lss_em.txt -F sf/t /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/20-08-2014/3/1/evVarFriend_{cname}.root --tree treeProducerSusyMultilepton --s2v
+		mv roc_2lss_em.png roc_2lss_em.root /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/20-08-2014/4/1/2
+		;;
+	3 )
+		python rocCurves.py -P /afs/cern.ch/work/g/gpetrucc/TREES_53X_170714 -j 6 -f -l 19.6 mca-2lss-dataBCat4Plots.txt bins/2lss_ee.txt roc_2lss_ee.txt -F sf/t /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/20-08-2014/3/1/evVarFriend_{cname}.root --tree treeProducerSusyMultilepton --s2v
+		mv roc_2lss_ee.png roc_2lss_ee.root /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/20-08-2014/4/1/3
+		;;
+	4 )
+		python rocCurves.py -P /afs/cern.ch/work/g/gpetrucc/TREES_53X_170714 -j 6 -f -l 19.6 mca-2lss-dataBCat4Plots.txt bins/3l_tight.txt roc_3l.txt -F sf/t /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/20-08-2014/3/1/evVarFriend_{cname}.root --tree treeProducerSusyMultilepton --s2v
+		mv roc_3l.png roc_3l.root /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/20-08-2014/4/1/4
+		;;
+	5 )
+		python rocCurves.py -P /afs/cern.ch/work/g/gpetrucc/TREES_53X_170714 -j 6 -f -l 19.6 mca-2lss-dataBCat4Plots.txt bins/4l.txt roc_4l.txt -F sf/t /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/20-08-2014/3/1/evVarFriend_{cname}.root --tree treeProducerSusyMultilepton --s2v
+		mv roc_4l.png roc_4l.root /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/20-08-2014/4/1/5
+		;;
+esac 
