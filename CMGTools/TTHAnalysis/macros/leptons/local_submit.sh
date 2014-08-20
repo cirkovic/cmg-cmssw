c3da9952987cd1ad1be7ea4b74cf6a5d1ad76a33
cirkovic <predrag.cirkovic@cern.ch>
2014-08-20 21:16:41 +0200

    Loading environment added at the beginning of the local_submit.sh

--- CMGTools/TTHAnalysis/macros/leptons/local_submit.sh
+++ CMGTools/TTHAnalysis/macros/leptons/local_submit.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+cd /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/CMSSW_5_3_19/src
+eval `scramv1 runtime -sh`
+cd /afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/CMSSW_5_3_19/src/CMGTools/TTHAnalysis/macros/leptons
+
 case $1 in
 	1 )
 		root -l -q 'trainLeptonID.cxx("mu_pteta_high_b")' ;;
