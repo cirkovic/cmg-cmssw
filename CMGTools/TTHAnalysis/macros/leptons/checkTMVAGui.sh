f7c4da066bfdc3f08353c026a4ecdc5c5484ea6e
cirkovic <predrag.cirkovic@cern.ch>
2014-08-21 11:13:36 +0200

    checkTMVAGui.sh parametrized in input path and various TMVAGui arguments.

--- CMGTools/TTHAnalysis/macros/leptons/checkTMVAGui.sh
+++ CMGTools/TTHAnalysis/macros/leptons/checkTMVAGui.sh
@@ -6,6 +6,22 @@ then path=$1
 else path=/afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/21-08-2014/2/1
 fi
 
-root -l '/afs/cern.ch/sw/lcg/app/releases/ROOT/5.28.00g/x86_64-slc5-gcc43-opt/root/tmva/test/TMVAGui.C("'$path'/mu_pteta_high_b.root")'
+if [[ $1 != "" ]]
+then
+	case $1 in
+		1 )  arg=mu_pteta_low_b.root   ;;
+		2 )  arg=mu_pteta_high_b.root  ;;
+		3 )  arg=mu_pteta_low_e.root   ;;
+		4 )  arg=mu_pteta_high_e.root  ;;
+		5 )  arg=el_pteta_low_cb.root  ;;
+		6 )  arg=el_pteta_low_fb.root  ;;
+		7 )  arg=el_pteta_low_ec.root  ;;
+		8 )  arg=el_pteta_high_cb.root ;;
+		9 )  arg=el_pteta_high_fb.root ;;
+		10 ) arg=el_pteta_high_ec.root ;;
+	esac	
+else path=/afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/21-08-2014/2/1
+fi
 
+root -l '/afs/cern.ch/sw/lcg/app/releases/ROOT/5.28.00g/x86_64-slc5-gcc43-opt/root/tmva/test/TMVAGui.C("'$path'/'$arg'")'
 
