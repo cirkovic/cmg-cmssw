f1c1b8f4695bfa2c1503c34ebf0983efbfa22ce5
cirkovic <predrag.cirkovic@cern.ch>
2014-08-25 23:24:28 +0200

    calls with {mm, me, ee} argument added to {ttW, ttbar, mix}.

--- CMGTools/TTHAnalysis/macros/finalMVA/script.sh
+++ CMGTools/TTHAnalysis/macros/finalMVA/script.sh
@@ -1,6 +1,6 @@
 ch=2lss
 
-for arg in ttW ttbar mix
+for arg in ttW ttbar mix ttW_mm ttbar_mm mix_mm ttW_me ttbar_me mix_me ttW_ee ttbar_ee mix_ee
 do
 	root -l -q $ch/'trainMVA_'$ch'.cxx("'$arg'")'
 	mkdir -p output/$ch/$arg
