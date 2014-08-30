229399326043f977a8fb4178c452749271cf5b44
cirkovic <predrag.cirkovic@cern.ch>
2014-08-30 23:14:50 +0200

    ttbarEventReco_2lss.py adaptation for TREES_53X_170714 in progress...

--- CMGTools/TTHAnalysis/macros/prepareEventVariablesFriendTree.py
+++ CMGTools/TTHAnalysis/macros/prepareEventVariablesFriendTree.py
@@ -5,8 +5,8 @@ import os.path
 
 MODULES = []
 
-from CMGTools.TTHAnalysis.tools.eventVars_2lss import EventVars2LSS 
-MODULES.append( ('2lss', EventVars2LSS()) )
+#from CMGTools.TTHAnalysis.tools.eventVars_2lss import EventVars2LSS 
+#MODULES.append( ('2lss', EventVars2LSS()) )
 #from CMGTools.TTHAnalysis.tools.finalMVA_2lss import FinalMVA_2LSS
 #MODULES.append( ('2lss_mva', FinalMVA_2LSS()) )
 #from CMGTools.TTHAnalysis.tools.finalMVA_3l import FinalMVA_3L
@@ -15,11 +15,11 @@ MODULES.append( ('2lss', EventVars2LSS()) )
 #MODULES.append( ('bbvars', bbVars()) )
 #from CMGTools.TTHAnalysis.tools.finalMVA_2lss_2 import FinalMVA_2LSS_2
 #MODULES.append( ('finalMVA_2lss_2', FinalMVA_2LSS_2()) )
-#from CMGTools.TTHAnalysis.tools.ttbarEventReco_2lss import TTEventReco_MC
-#MODULES.append( ('ttreco_mc', TTEventReco_MC()) )
-#from CMGTools.TTHAnalysis.tools.ttbarEventReco_2lss import TTEventReco
-#MODULES.append( ('ttreco', TTEventReco(sortersToUse={"BestGuess":"", "BestBySum4NoTJJb":"_bySum4"})) )
-#MODULES.append( ('ttreco', TTEventReco(sortersToUse={"BestGuess":"","ByGuessLL2B":"_byLL"})) )
+from CMGTools.TTHAnalysis.tools.ttbarEventReco_2lss import TTEventReco_MC
+MODULES.append( ('ttreco_mc', TTEventReco_MC()) )
+from CMGTools.TTHAnalysis.tools.ttbarEventReco_2lss import TTEventReco
+MODULES.append( ('ttreco', TTEventReco(sortersToUse={"BestGuess":"", "BestBySum4NoTJJb":"_bySum4"})) )
+MODULES.append( ('ttreco', TTEventReco(sortersToUse={"BestGuess":"","ByGuessLL2B":"_byLL"})) )
 #from CMGTools.TTHAnalysis.tools.LepMVAFriend import LepMVAFriend
 #MODULES.append( ('LepMVAFriend', LepMVAFriend("/afs/cern.ch/work/c/cirkovic/Milos_13-08-2014/27-08-2014/2")) )
 
