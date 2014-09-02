04f2bb9744929858a83ccbca113f3e472c0c2d83
cirkovic <predrag.cirkovic@cern.ch>
2014-09-02 08:21:23 +0200

    Adding the script to run various trainigs.

---
+++ CMGTools/TTHAnalysis/macros/finalMVA/script.sh
@@ -0,0 +1,12 @@
+ch=2lss
+
+for NVAR in 6VAR 7VAR 9VAR
+do
+	for arg in ttbar ttW mix
+	do
+		root -l -q $ch/'trainMVA_'$ch'_'$NVAR'.cxx("'$arg'")'
+		mkdir -p $ch/output/$NVAR/$arg
+		mv $arg.root weights $ch/output/$NVAR/$arg
+	done
+done
+
