e43725e4cf2e0ae21ee1271b041f3c94a6fe62fd
cirkovic <predrag.cirkovic@cern.ch>
2014-09-03 23:09:34 +0200

    Script for running trainLeptonID.cxx on various trainTTH* signal and trainTTJets* background root files.

---
+++ CMGTools/TTHAnalysis/macros/leptons/local_submit.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+cd /afs/cern.ch/work/c/cirkovic/Milos_03-09-2014/CMSSW_7_0_6_patch1/src
+eval `scramv1 runtime -sh`
+cd /afs/cern.ch/work/c/cirkovic/Milos_03-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/macros/leptons
+
+trainTTH=( trainTTH_PU20bx25.root trainTTHpy6_S14.root )
+trainTTJets=( trainTTJets_PU20bx25.root trainTTJets_PUS14.root )
+
+case $1 in
+   1 )
+      root.exe -b -l -q "${trainTTH[$2]}" "${trainTTJets[$2]}" 'trainLeptonID.cxx("mu_pteta_high_b")' ;;
+   2 )
+      root.exe -b -l -q "${trainTTH[$2]}" "${trainTTJets[$2]}" 'trainLeptonID.cxx("mu_pteta_low_b")' ;;
+   3 )
+      root.exe -b -l -q "${trainTTH[$2]}" "${trainTTJets[$2]}" 'trainLeptonID.cxx("mu_pteta_high_e")' ;;
+   4 )
+      root.exe -b -l -q "${trainTTH[$2]}" "${trainTTJets[$2]}" 'trainLeptonID.cxx("mu_pteta_low_e")' ;;
+   5 )
+      root.exe -b -l -q "${trainTTH[$2]}" "${trainTTJets[$2]}" 'trainLeptonID.cxx("el_pteta_high_cb")' ;;
+   6 )
+      root.exe -b -l -q "${trainTTH[$2]}" "${trainTTJets[$2]}" 'trainLeptonID.cxx("el_pteta_low_cb")' ;;
+   7 )
+      root.exe -b -l -q "${trainTTH[$2]}" "${trainTTJets[$2]}" 'trainLeptonID.cxx("el_pteta_high_fb")' ;;
+   8 )
+      root.exe -b -l -q "${trainTTH[$2]}" "${trainTTJets[$2]}" 'trainLeptonID.cxx("el_pteta_low_fb")' ;;
+   9 )
+      root.exe -b -l -q "${trainTTH[$2]}" "${trainTTJets[$2]}" 'trainLeptonID.cxx("el_pteta_high_ec")' ;;
+   10 )
+      root.exe -b -l -q "${trainTTH[$2]}" "${trainTTJets[$2]}" 'trainLeptonID.cxx("el_pteta_low_ec")' ;;
+esac
+
