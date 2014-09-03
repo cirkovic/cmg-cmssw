58d0872d13bd9c520d5cf45010f1ed3eeef8a566
cirkovic <predrag.cirkovic@cern.ch>
2014-09-03 23:05:16 +0200

    Script for running prepareLepTrain.py to produce various trainTTH*.root and trainTTJets*.root files.

--- CMGTools/TTHAnalysis/macros/leptons/runVariousPrepareLepTrain.sh
+++ CMGTools/TTHAnalysis/macros/leptons/runVariousPrepareLepTrain.sh
@@ -1,5 +1,8 @@
-for i in `ls /afs/cern.ch/work/c/cirkovic/Milos_03-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/25ns`
+for j in 25ns 50ns
 do
-    python prepareLepTrain.py /afs/cern.ch/work/c/cirkovic/Milos_03-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/25ns/$i train$i.root --vector --tree treeProducerSusyMultilepton
+	for i in `ls /afs/cern.ch/work/c/cirkovic/Milos_03-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/$j`
+	do
+   	 python prepareLepTrain.py /afs/cern.ch/work/c/cirkovic/Milos_03-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/$j/$i train$i.root --vector --tree treeProducerSusyMultilepton
+	done
 done
 
