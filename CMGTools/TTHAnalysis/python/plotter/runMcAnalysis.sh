9cbce75f1d2b43a7178b98a315141ef6361755b5
cirkovic <predrag.cirkovic@cern.ch>
2014-09-04 21:54:08 +0200

    Scripts for running mcAnalysis.py without any MVA cut.

---
+++ CMGTools/TTHAnalysis/python/plotter/runMcAnalysis.sh
@@ -0,0 +1,6 @@
+python mcAnalysis.py mca_sh.txt bins/2lss_mumu.txt -X 'lep MVA' -P /afs/cern.ch/work/c/cirkovic/Milos_03-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/Output_Directory -l 19.6 -j 6 --s2v --tree treeProducerSusyMultilepton --s2v
+
+python mcAnalysis.py mca_sh.txt bins/2lss_ee.txt -X 'lep MVA' -P /afs/cern.ch/work/c/cirkovic/Milos_03-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/Output_Directory -l 19.6 -j 6 --s2v --tree treeProducerSusyMultilepton --s2v
+
+python mcAnalysis.py mca_sh.txt bins/2lss_em.txt -X 'lep MVA' -P /afs/cern.ch/work/c/cirkovic/Milos_03-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/Output_Directory -l 19.6 -j 6 --s2v --tree treeProducerSusyMultilepton --s2v
+
