d1a0a0ed407db29de13b1aef237261c6e4470521
cirkovic <predrag.cirkovic@cern.ch>
2014-09-05 11:43:06 +0200

    Input files and order of arguments changed.

--- CMGTools/TTHAnalysis/macros/leptons/local_submit.sh
+++ CMGTools/TTHAnalysis/macros/leptons/local_submit.sh
@@ -4,30 +4,32 @@ cd /afs/cern.ch/work/c/cirkovic/Milos_03-09-2014/CMSSW_7_0_6_patch1/src
 eval `scramv1 runtime -sh`
 cd /afs/cern.ch/work/c/cirkovic/Milos_03-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/macros/leptons
 
-trainTTH=( trainTTH_PU20bx25.root trainTTHpy6_S14.root )
-trainTTJets=( trainTTJets_PU20bx25.root trainTTJets_PUS14.root )
+INPUT_PATH=/afs/cern.ch/work/c/cirkovic/05-09-2014/
 
-echo train/"${trainTTH[$2]}" train/"${trainTTJets[$2]}"
+trainTTH=( $INPUT_PATH/25ns/trainTTHWW_25ns.root $INPUT_PATH/50ns/trainTTHWW_50ns.root )
+trainTTJets=( $INPUT_PATH/25ns/trainTTJets_25ns.root $INPUT_PATH/50ns/trainTTJets_50ns.root )
 
-case $1 in
+echo "${trainTTH[$1]}" "${trainTTJets[$1]}"
+
+case $2 in
    0 )
-      root.exe -b -l -q train/"${trainTTH[$2]}" train/"${trainTTJets[$2]}" 'trainLeptonID.cxx("mu_pteta_high_b")' ;;
+      root.exe -b -l -q "${trainTTH[$1]}" "${trainTTJets[$1]}" 'trainLeptonID.cxx("mu_pteta_high_b")' ;;
    1 )
-      root.exe -b -l -q train/"${trainTTH[$2]}" train/"${trainTTJets[$2]}" 'trainLeptonID.cxx("mu_pteta_low_b")' ;;
+      root.exe -b -l -q "${trainTTH[$1]}" "${trainTTJets[$1]}" 'trainLeptonID.cxx("mu_pteta_low_b")' ;;
    2 )
-      root.exe -b -l -q train/"${trainTTH[$2]}" train/"${trainTTJets[$2]}" 'trainLeptonID.cxx("mu_pteta_high_e")' ;;
+      root.exe -b -l -q "${trainTTH[$1]}" "${trainTTJets[$1]}" 'trainLeptonID.cxx("mu_pteta_high_e")' ;;
    3 )
-      root.exe -b -l -q train/"${trainTTH[$2]}" train/"${trainTTJets[$2]}" 'trainLeptonID.cxx("mu_pteta_low_e")' ;;
+      root.exe -b -l -q "${trainTTH[$1]}" "${trainTTJets[$1]}" 'trainLeptonID.cxx("mu_pteta_low_e")' ;;
    4 )
-      root.exe -b -l -q train/"${trainTTH[$2]}" train/"${trainTTJets[$2]}" 'trainLeptonID.cxx("el_pteta_high_cb")' ;;
+      root.exe -b -l -q "${trainTTH[$1]}" "${trainTTJets[$1]}" 'trainLeptonID.cxx("el_pteta_high_cb")' ;;
    5 )
-      root.exe -b -l -q train/"${trainTTH[$2]}" train/"${trainTTJets[$2]}" 'trainLeptonID.cxx("el_pteta_low_cb")' ;;
+      root.exe -b -l -q "${trainTTH[$1]}" "${trainTTJets[$1]}" 'trainLeptonID.cxx("el_pteta_low_cb")' ;;
    6 )
-      root.exe -b -l -q train/"${trainTTH[$2]}" train/"${trainTTJets[$2]}" 'trainLeptonID.cxx("el_pteta_high_fb")' ;;
+      root.exe -b -l -q "${trainTTH[$1]}" "${trainTTJets[$1]}" 'trainLeptonID.cxx("el_pteta_high_fb")' ;;
    7 )
-      root.exe -b -l -q train/"${trainTTH[$2]}" train/"${trainTTJets[$2]}" 'trainLeptonID.cxx("el_pteta_low_fb")' ;;
+      root.exe -b -l -q "${trainTTH[$1]}" "${trainTTJets[$1]}" 'trainLeptonID.cxx("el_pteta_low_fb")' ;;
    8 )
-      root.exe -b -l -q train/"${trainTTH[$2]}" train/"${trainTTJets[$2]}" 'trainLeptonID.cxx("el_pteta_high_ec")' ;;
+      root.exe -b -l -q "${trainTTH[$1]}" "${trainTTJets[$1]}" 'trainLeptonID.cxx("el_pteta_high_ec")' ;;
    9 )
-      root.exe -b -l -q train/"${trainTTH[$2]}" train/"${trainTTJets[$2]}" 'trainLeptonID.cxx("el_pteta_low_ec")' ;;
+      root.exe -b -l -q "${trainTTH[$1]}" "${trainTTJets[$1]}" 'trainLeptonID.cxx("el_pteta_low_ec")' ;;
 esac
