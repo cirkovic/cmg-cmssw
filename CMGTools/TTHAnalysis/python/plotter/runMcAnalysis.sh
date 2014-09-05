8335070d928ec9baa79aa25e46877f6a71c7a8f6
cirkovic <predrag.cirkovic@cern.ch>
2014-09-05 19:06:04 +0200

    Modifications to run mcAnalysis and plot ROC curves.

---
+++ CMGTools/TTHAnalysis/python/plotter/runMcAnalysis.sh
@@ -0,0 +1,6 @@
+python mcAnalysis.py mca_sh.txt -p ttHWW bins/2lss_mumu.txt -X 'lep MVA' -P /afs/cern.ch/work/c/cirkovic/TREES_53X_170714_1 -l 19.6 -j 6 --s2v --tree treeProducerSusyMultilepton
+
+python mcAnalysis.py mca_sh.txt -p ttHWW bins/2lss_ee.txt -X 'lep MVA' -P /afs/cern.ch/work/c/cirkovic/TREES_53X_170714_1 -l 19.6 -j 6 --s2v --tree treeProducerSusyMultilepton
+
+python mcAnalysis.py mca_sh.txt -p ttHWW bins/2lss_em.txt -X 'lep MVA' -P /afs/cern.ch/work/c/cirkovic/TREES_53X_170714_1 -l 19.6 -j 6 --s2v --tree treeProducerSusyMultilepton
+
