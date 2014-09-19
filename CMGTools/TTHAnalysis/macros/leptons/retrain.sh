f2f8813c8244803489dcd7acce2603859142043f
cirkovic <predrag.cirkovic@cern.ch>
2014-09-19 11:52:57 +0200

    Retraining for (TTHnlo_PU20bx25, TTJets_PU20bx25) and (TTHnlo_S14, TTJets_PUS14).

---
+++ CMGTools/TTHAnalysis/macros/leptons/retrain.sh
@@ -0,0 +1,6 @@
+python prepareLepTrain.py /afs/cern.ch/work/c/cirkovic/Milos_15-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/OUTPUT_TTJets_trigOff/TTHnlo_PU20bx25 trainTTHnlo_PU20bx25.root --tree treeProducerSusyMultilepton
+python prepareLepTrain.py /afs/cern.ch/work/c/cirkovic/Milos_15-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/OUTPUT_TTJets_trigOff/TTJets_PU20bx25 trainTTJets_PU20bx25.root --tree treeProducerSusyMultilepton
+
+python prepareLepTrain.py /afs/cern.ch/work/c/cirkovic/Milos_15-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/OUTPUT_TTJets_trigOff/TTHnlo_S14 trainTTHnlo_S14.root --tree treeProducerSusyMultilepton
+python prepareLepTrain.py /afs/cern.ch/work/c/cirkovic/Milos_15-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/OUTPUT_TTJets_trigOff/TTJets_PUS14 trainTTJets_PUS14.root --tree treeProducerSusyMultilepton
+
---
+++ CMGTools/TTHAnalysis/macros/leptons/retrain_100000.sh
@@ -0,0 +1,6 @@
+python prepareLepTrain.py /afs/cern.ch/work/c/cirkovic/Milos_15-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/OUTPUT_TTJets_trigOff/TTHnlo_PU20bx25 trainTTHnlo_PU20bx25_100000.root --tree treeProducerSusyMultilepton 1 100000
+python prepareLepTrain.py /afs/cern.ch/work/c/cirkovic/Milos_15-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/OUTPUT_TTJets_trigOff/TTJets_PU20bx25 trainTTJets_PU20bx25_100000.root --tree treeProducerSusyMultilepton 1 100000
+
+python prepareLepTrain.py /afs/cern.ch/work/c/cirkovic/Milos_15-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/OUTPUT_TTJets_trigOff/TTHnlo_S14 trainTTHnlo_S14_100000.root --tree treeProducerSusyMultilepton 1 100000
+python prepareLepTrain.py /afs/cern.ch/work/c/cirkovic/Milos_15-09-2014/CMSSW_7_0_6_patch1/src/CMGTools/TTHAnalysis/cfg/OUTPUT_TTJets_trigOff/TTJets_PUS14 trainTTJets_PUS14_100000.root --tree treeProducerSusyMultilepton 1 100000
+
