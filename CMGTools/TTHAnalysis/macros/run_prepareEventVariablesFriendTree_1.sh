6fea3a2a5a744f34e7d7e7296599611273f7a087
cirkovic <predrag.cirkovic@cern.ch>
2014-09-25 21:11:33 +0200

    Training for 23j/4j categories redone.

--- CMGTools/TTHAnalysis/macros/run_prepareEventVariablesFriendTree_1.sh
+++ CMGTools/TTHAnalysis/macros/run_prepareEventVariablesFriendTree_1.sh
@@ -2,6 +2,6 @@
 
 TF=`date +"%d%m%y_%H%M%S"`
 mkdir FT_1_OUTPUT_$TF
-python prepareEventVariablesFriendTree_1.py -q 8nh -t ttHLepTreeProducerBase -F sf/t /afs/cern.ch/work/c/cirkovic/Categorization_240914/CMSSW_5_3_19/src/CMGTools/TTHAnalysis/macros/FT_OUTPUT_240914_170333/evVarFriend_{cname}.root /afs/cern.ch/work/c/cirkovic/TREES_250513_HADD FT_1_OUTPUT_$TF > bsub_1_all_$TF.sh
+python prepareEventVariablesFriendTree_1.py -q 8nh -N 50000 -t ttHLepTreeProducerBase -F sf/t /afs/cern.ch/work/c/cirkovic/Categorization_240914/CMSSW_5_3_19/src/CMGTools/TTHAnalysis/macros/FT_OUTPUT_240914_170333/evVarFriend_{cname}.root /afs/cern.ch/work/c/cirkovic/TREES_250513_HADD_DATA FT_1_OUTPUT_$TF > bsub_1_all_$TF.sh
 chmod +x bsub_1_all_$TF.sh
 
--- CMGTools/TTHAnalysis/macros/run_prepareEventVariablesFriendTree_data_1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-TF=`date +"%d%m%y_%H%M%S"`
-mkdir FT_DATA_1_OUTPUT_$TF
-python prepareEventVariablesFriendTree_1.py -q 8nh -N 50000 -t ttHLepTreeProducerBase -F sf/t /afs/cern.ch/work/c/cirkovic/Categorization_240914/CMSSW_5_3_19/src/CMGTools/TTHAnalysis/macros/FT_DATA_OUTPUT_250914_103448/evVarFriend_{cname}.root /afs/cern.ch/work/c/cirkovic/TREES_250513_HADD_DATA FT_DATA_1_OUTPUT_$TF > bsub_data_1_all_$TF.sh
-chmod +x bsub_data_1_all_$TF.sh
-
